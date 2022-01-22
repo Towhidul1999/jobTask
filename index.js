2b039c062c77af055a8c7a165763534e15fe3f7f
Towhidul1999 <towhidulsislam@gmail.com>
2022-01-22 16:46:50 +0600

    task added

---
+++ index.js
@@ -0,0 +1,18 @@
+function showMenu() {
+    var bar=document.getElementById("bar");
+    var time=document.getElementById("time");
+    var menu =document.getElementById("mobileM");
+    bar.style.display="none";
+    time.style.display="block";
+    menu.style.display="block";
+
+}
+function closeMenu() {
+    var bar=document.getElementById("bar");
+    var time=document.getElementById("time");
+    var menu =document.getElementById("mobileM");
+    bar.style.display="block";
+    time.style.display="none";
+    menu.style.display="none";
+
+}
